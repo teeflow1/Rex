7e42cb07a0b3b188b8cebda0c8a63a26ab34d697
“Temitope <temtopeayobami@gmail.com>
2023-07-09 17:15:47 +0100

    Worked on the contact sec

--- music_app/views.py
+++ music_app/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'apps/home.html', {})
+    if request.method == "POST":
+        contact_name = request.POST['contact-name']
+        contact_email = request.POST['contact-email']
+        contact_company = request.POST['contact-company']
+        contact_message = request.POST['contact-message']
+        
+        return render(request, 'apps/home.html', {'contact_name':contact_name})
+    else:
+        return render(request, 'apps/home.html', {})
+        
+        
+        
+        
+  
+
+
 
 def ticket(request):
     return render(request, 'apps/ticket.html', {})
