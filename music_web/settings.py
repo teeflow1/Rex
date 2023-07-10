9cbe07728456c2b0c8cf6356dc1636c7ba8c6c71
“Temitope <temtopeayobami@gmail.com>
2023-07-10 14:39:36 +0100

    Initial commit

--- music_web/settings.py
+++ music_web/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -145,5 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
+    'default': dj_database_url.parse(env('DATABASES_URL'))
 }
