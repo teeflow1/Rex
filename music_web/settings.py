7bff95ad8c6753d0d551043d19d6e3811d58061b
“Temitope <temtopeayobami@gmail.com>
2023-08-01 14:48:10 +0100

    Done

--- music_web/settings.py
+++ music_web/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-
-
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,8 +157,8 @@ DATABASES = {
 # Email Settings 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER= 'temitopeayobami995@gmail.com'
-EMAIL_HOST_PASSWORD =  'mqtvpmzeawitkwel' #app password
-EMAIL_USE_TLS = True
-#EMAIL_USE_SSL = False
+EMAIL_PORT = '465'
+EMAIL_HOST_USER= os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =  os.environ.get('EMAIL_HOST_PASSWORD') #app password
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
