a5fc04e43a86c758304ff74660a2ff761fe8af7b
“Temitope <temtopeayobami@gmail.com>
2023-08-01 15:25:20 +0100

    done

--- music_web/settings.py
+++ music_web/settings.py
@@ -160,5 +160,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER= 'temitopeayobami995@gmail.com'
 EMAIL_HOST_PASSWORD =  'mqtvpmzeawitkwel' #app password
-EMAIL_USE_TLS = True
-#EMAIL_USE_SSL = True
+#EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
