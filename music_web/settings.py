0b92645a308b3dd27ed7937c6e3a85bffb0729c1
“Temitope <temtopeayobami@gmail.com>
2023-08-01 14:06:37 +0100

    Removed

--- music_web/settings.py
+++ music_web/settings.py
@@ -13,6 +13,11 @@ import os
 from pathlib import Path
 
 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -150,9 +155,10 @@ DATABASES = {
 }
 
 # Email Settings 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER= 'temitopeayobami995@gmail.com'
-EMAIL_HOST_PASSWORD = 'mqtvpmzeawitkwel'
+EMAIL_HOST_USER= os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') #app password
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
