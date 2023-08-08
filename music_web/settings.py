0aefc8921ea62a913b27b2274213d170a1a13a1c
“Temitope <temtopeayobami@gmail.com>
2023-08-08 12:17:20 +0100

    Environ

--- music_web/settings.py
+++ music_web/settings.py
@@ -171,9 +171,9 @@ DATABASES = {
 
 # Email Settings 
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = '587'
-EMAIL_HOST_USER= 'temitopeayobami995@gmail.com'
-EMAIL_HOST_PASSWORD =  'mqtvpmzeawitkwel' #app password
+EMAIL_HOST_USER= os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') #app password
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = True
